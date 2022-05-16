caea0debc0f67f02a9fbc5b5a8e4e58b621ad1e4
Aaron Eljas <aaron.eljas@student.nhlstenden.com>
2022-05-16 15:40:11 +0200

    Starting commit
    Added 32feet.NET NuGet package for classic bluetooth
    
    Added some code to loop through all com ports and display them

---
+++ Desktop/Desktop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Management;
+
+namespace Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SELECT ALL COM PORTS FROM Win32_PnPEntity
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portnames = SerialPort.GetPortNames(); //store all the port names
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                var portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
+
+                foreach (string s in portList)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
