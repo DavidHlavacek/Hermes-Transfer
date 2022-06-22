65f6df0daaf1ea864881eadc4cf7e30ede20796c
Nyximus32 <miroslav.penchev@student.nhlstenden.com>
2022-06-22 21:41:13 +0200

    Pyhon/C# application to get the path ot the focused file

---
+++ Desktop/Python/Test/testing.py
@@ -0,0 +1,21 @@
+import os
+import sys
+file_ = sys.argv[1]
+for root, dirs, files in os.walk('C:/'):
+    for name in files:
+        name2 = name.split('.')[0]
+        extension = name.split('.')[-1].lower()
+        if file_ == name2 and extension != "lnk":
+            print(os.path.abspath(os.path.join(root, name)))
+#             # if file_ in ".*":
+#             #     break;
+
+
+
+
+
+
+# test = "C-Doc-Ha-lol.exe"
+# test2 = test.split('-')[-1]
+# test3 = test2.split('.')[0]
+# print(test3)
